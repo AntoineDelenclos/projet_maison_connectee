2e3da20391e2339b542b630f255b7318f96d09f9
Dastu6 <119614582+Dastu6@users.noreply.github.com>
2023-01-22 23:43:25 +0100

    Update modif_appart.php

--- Projet/modif_appart.php
+++ Projet/modif_appart.php
@@ -12,4 +12,32 @@
     <title>Maison Connectée</title>
 </head>
 
+<?php
+$_SESSION["type_app"]="Electronique";
+$sql="INSERT INTO APPAREIL (`Nom_appareil`, `Type_appareil`, `Libel_appareil`, `Id_appart`) VALUES ('$_POST["lampe"]','$_SESSION["type_app"]','$_POST["piece"]','$_SESSION["id_app"]');";
+$result=mysqli_query($con,$sql);
+
+?>
+
 <body>
+<div>
+        
+        <p>Ajout d'appareil</p>
+        <form method="POST" action="modif_appart.php">
+            <fieldset>
+                <label for="lampe">Lampe</label>
+                <input class="chatbox" type="text" id="lampe" name="lampe">
+                <br>
+                <label for="tv">Télévision</label>
+                <input class="chatbox" type="text" id="tv" name="tv">
+                <br>
+                <label for="piece">Pièce</label>
+                <input class="chatbox" type="text" id="piece" name="piece">
+                <br>              
+            </fieldset>
+
+            <button type="submit" name="submit">Ajouter</button>
+        </form>
+        
+
+    </div>
