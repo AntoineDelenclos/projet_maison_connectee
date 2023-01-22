d613ec461add7ebbd6bc86fc0a2aad60813c31e8
XayAnthony <120203775+XayAnthony@users.noreply.github.com>
2023-01-22 19:05:22 +0100

    Delete Projet directory

--- Projet/debug.php
+++
@@ -1,3 +0,0 @@
-<?php session_start();?>
-<?php include 'bdd_connect.php';?>
-<?php phpinfo();?>
--- Projet/disconnect.php
+++
@@ -1,8 +0,0 @@
-<?php session_start();?>
-<?php include 'bdd_connect.php';?>
-
-<?php session_destroy();
-
-header("Location: ./index.php?disconnect=success");
-
-?>
